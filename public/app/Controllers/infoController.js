8579045fd20527060a488e0047a84b6baa9b365c
krichdev <kgrichardson2@gmail.com>
2017-04-07 11:03:11 -0700

    added project db and posting project

--- public/app/Controllers/infoController.js
+++ public/app/Controllers/infoController.js
@@ -1,29 +1,28 @@
 angular.module('App')
 .controller('InfoCtrl', ['$scope', '$http', '$state', 'Auth', 'UserFactory', '$stateParams', 'ProjectFactory', function($scope, $http, $state, Auth, UserFactory, $stateParams, ProjectFactory) {
     $scope.user;
+    $scope.userId = Auth.currentUser().id
 
-    UserFactory.getUser($stateParams.id)
+    UserFactory.getUser($scope.userId)
     .then(
         function success(res){
             $scope.user = res.data
-            ProjectFactory.getAllProjects().then(
-                function success(res){
-                    $scope.projects= {
-                        projectTitle: '',
-                        projectBody: '',
-                        projectDate: '',
-                        projectPoster: res.data.user.id,
-                        projectPrice: '',
-                        projectPic: '',
-                    };
-                }
-            ),
-            function error(err){console.log('error')}
     },
         function error(err){console.log('error')}
     )
 
     $scope.createProject = function() {
+        $scope.projects = {
+            projectTitle: $scope.project.projectTitle,
+            projectBody: $scope.project.projectBody,
+            projectDate: $scope.project.projectDate,
+            projectPrice: $scope.project.projectPrice,
+            projectPic: $scope.project.projectPic,
+            projectPoster: $scope.userId
+
+        }
+
+
         $http.post('/api/projects', $scope.projects).then(function success(res){
             console.log($scope.projects)
             console.log(res)
